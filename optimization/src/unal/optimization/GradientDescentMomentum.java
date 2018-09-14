0d8e5a800ed56a8f6c9d9d9d6b5c5bed46448155
Edward Carrillo <eccarrilloe@gmail.com>
2018-09-14 06:17:12 -0500

    feat(gradientDescendentMomentun): gradientDescendentMomentum working

--- optimization/src/unal/optimization/GradientDescentMomentum.java
+++ optimization/src/unal/optimization/GradientDescentMomentum.java
@@ -3,7 +3,38 @@ package unal.optimization;
 import unal.functions.OptimizableFunction;
 
 public class GradientDescentMomentum implements Optimizable {
+  private int iterations;
+  private double alpha;
+  private double miu;
+
+  public GradientDescentMomentum(double alpha, double miu, int iterations) {
+    this.alpha = alpha;
+    this.miu = miu;
+    this.iterations = iterations;
+  }
+
   public double[] optimize(OptimizableFunction f) {
-    return null;
+    double[] x0 = f.getInitialPoint();
+    double[] delta = new double[f.getDimensions()];
+
+    for (int i = 0; i < delta.length; i++) {
+      delta[i] = 0;
+    }
+
+    for (int i = 0; i < this.iterations; i++) {
+      double[] x1 = x0.clone();
+      double[] gradient = f.gradient(x1);
+
+      for (int j = 0; j < x1.length; j++) {
+        delta[j] = miu * delta[j] - gradient[j] * alpha;
+        x1[j] += delta[j];
+      }
+
+      System.out.println(String.format("#%d: [%.8f, %.8f] => %.8f", i + 1, x1[0], x1[1], f.f(x1)));
+
+      x0 = x1;
+    }
+
+    return x0;
   }
 }
