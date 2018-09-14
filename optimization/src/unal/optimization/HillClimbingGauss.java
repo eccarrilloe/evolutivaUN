7ca4764918a4e3f89cfeef77fdd5a6df86bd5397
Edward Carrillo <eccarrilloe@gmail.com>
2018-09-14 06:43:42 -0500

    feat(HillClimbingGauss): HillClimbingGauss working

--- optimization/src/unal/optimization/HillClimbingGauss.java
+++ optimization/src/unal/optimization/HillClimbingGauss.java
@@ -1,9 +1,38 @@
 package unal.optimization;
 
 import unal.functions.OptimizableFunction;
+import java.util.Random;
 
 public class HillClimbingGauss implements Optimizable {
+
+  private double sigma;
+  private int iterations;
+  private Random random;
+
+  public HillClimbingGauss(double sigma, int iterations) {
+    this.sigma = sigma;
+    this.iterations = iterations;
+    this.random = new Random();
+  }
+
   public double[] optimize(OptimizableFunction f) {
-    return null;
+    double[] x0 = f.getInitialPoint();
+
+    for (int i = 0; i < this.iterations; i++) {
+      double[] x1 = x0.clone();
+
+      for (int j = 0; j < x1.length; j++) {
+          double ran = this.random.nextGaussian() * this.sigma;
+          x1[j] += ran;
+      }
+
+      if (f.f(x1) <= f.f(x0)) {
+          x0 = x1;
+      }
+
+      System.out.println(String.format("#%d: [%.8f, %.8f] => %.8f", i + 1, x0[0], x0[1], f.f(x0)));
+    }
+
+    return x0;
   }
 }
--- optimization/test/HillClimbingGaussTest.java
+++ optimization/test/HillClimbingGaussTest.java
@@ -1,7 +1,16 @@
 package test;
 
+import unal.functions.Rastrigin;
+import unal.optimization.HillClimbingGauss;
+
 class HillClimbingGaussTest {
   public static void main(String[] args) {
-    System.out.println("HelloWorld");
+    double sigma = .01;
+    int iterations = 1000;
+    HillClimbingGauss hillClimbing = new HillClimbingGauss(sigma, iterations);
+
+    Rastrigin f = new Rastrigin(2, 2);
+    double[] result = hillClimbing.optimize(f);
+    System.out.println(result[0] + " - " + result[1]);
   }
 }
